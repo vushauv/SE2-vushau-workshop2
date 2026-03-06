8875e1fb0fcc029ce268f8447e96084c6656eaa3
Vasili Vushau <vushauv@gmail.com>
2026-03-06 14:02:36 +0100

    feat: single number returns itself

--- string_calculator.py
+++ string_calculator.py
@@ -2,10 +2,13 @@
 
 class StringCalculator:
 
-    
+
     def calculate(self, input_string: str) -> int:
-            if not input_string:
-                  return 0
-            else:
+        if not input_string:
+                return 0
+        else:
+            try:
+                num = int(input_string)
+                return num
+            except:
                 raise NotImplementedError
-                  
