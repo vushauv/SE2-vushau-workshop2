7420c3dd8b4c1ffbb6944618a30856775ba93924
Vasili Vushau <vushauv@gmail.com>
2026-03-06 14:21:23 +0100

    feat: sum for two numbers delimited with commas

--- string_calculator.py
+++ string_calculator.py
@@ -1,14 +1,21 @@
-
-
 class StringCalculator:
 
+   def calculate(self, input_string: str) -> int:
+      if not input_string:
+         return 0
 
-    def calculate(self, input_string: str) -> int:
-        if not input_string:
-                return 0
-        else:
-            try:
-                num = int(input_string)
-                return num
-            except:
-                raise NotImplementedError
+      parsed_string: list[str] = input_string.split(",")
+
+      if len(parsed_string) == 1:
+         try:
+            num = int(input_string)
+            return num
+         except:
+            raise NotImplementedError
+      elif len(parsed_string) == 2:
+         try:
+            return int(parsed_string[0]) + int(parsed_string[1])
+         except:
+            raise NotImplementedError
+      else:
+         raise NotImplementedError
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -18,3 +18,14 @@ def test_empty_string_returns_zero():
 def test_single_number_returns_value(value: str) -> None:
     calc = StringCalculator()
     assert calc.calculate(value) == int(value)
+
+
+@pytest.mark.parametrize("num1,num2", [
+    (0, 0),
+    (-4, -2),
+    (10, 0),
+    (123, 456),
+])
+def test_two_numbers_comma_delimited_return_sum(num1: int, num2: int) -> None:
+    calc = StringCalculator()
+    assert calc.calculate(f"{num1},{num2}") == num1 + num2
