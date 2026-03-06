ae84e5c9668037cfa24ad98ab36d88e07bf63d65
Vasili Vushau <vushauv@gmail.com>
2026-03-06 13:59:56 +0100

    tests: create tests singleNumberReturnsNumber

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,5 +1,20 @@
+import pytest
 from string_calculator import StringCalculator
 
 def test_empty_string_returns_zero():
     calc = StringCalculator()
     assert calc.calculate("") == 0
+
+
+@pytest.mark.parametrize("value", [
+    "-20",
+    "-1"
+    "0",
+    "1",
+    "5",
+    "123",
+    "9999",
+])
+def test_single_number_returns_value(value: str) -> None:
+    calc = StringCalculator()
+    assert calc.calculate(value) == int(value)
